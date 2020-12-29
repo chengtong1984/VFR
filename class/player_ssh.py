72eb3b1fa57fea74126ceb9af19590c3e83017d8
鹉翎萌主 <58757427+chengtong1984@users.noreply.github.com>
2020-12-29 12:11:32 +0800

    Create player_ssh.py

---
+++ class/player_ssh.py
@@ -0,0 +1,62 @@
+import paramiko
+import time
+import os
+import sys
+import logging
+
+__author__ = 'sam.cheng'
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+rq = time.strftime('%Y%m%d%H%M', time.localtime(time.time()))
+log_path = os.path.dirname(os.getcwd()) + '/Logs/'
+log_name = log_path + rq + '.log'
+logfile = log_name
+fh = logging.FileHandler(logfile, mode='w')
+fh.setLevel(logging.DEBUG)
+formatter = logging.Formatter("%(asctime)s - %(filename)s[line:%(lineno)d] - %(levelname)s: %(message)s")
+fh.setFormatter(formatter)
+logger.addHandler(fh)
+
+pkey = '/home/STD-MO/.ssh/id_rsa'
+key = paramiko.RSAKey.from_private_key_file(pkey)
+
+def test_paramiko_interact(playerIP):
+    if(os.system('ping -c 1 -w 1 ' + str(playerIP)) == 0):
+        print('OK')
+        linux_command = 'sudo DMS-auto-update'
+        ssh = paramiko.SSHClient()
+        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        ssh.connect(str(playerIP),username = 'STD-MO',pkey = key,timeout = 900)
+        stdin,stdout,stderr = ssh.exec_command(linux_command)
+        value_size = stdout.readlines()
+        print(value_size)
+        for values in value_size:
+            if 'reboot' in values:
+                print(values)
+                stdin,stdout,stderr = ssh.exec_command('sudo reboot')
+                ssh.close()
+                logger.info(str(playerIP) + str(values))
+                return True
+            elif 'dl_failed' in values:
+                print(values)
+                logger.info(str(playerIP) + 'updating ended successfully')
+                ssh.close()
+                return False
+            elif 'removing' in values:
+                print(values)
+                logger.info(str(playerIP) + 'updating ended successfully')
+                ssh.close()
+                return False
+        ssh.close()
+        return True
+    else:
+        print ('connection failed')
+        logger.info(str(playerIP) + 'connection failed')
+        return True
+
+def update_one(playerlist):
+    result = True
+    while result:
+        result=test_paramiko_interact(playerlist)
+        time.sleep(10)
